d9dfa7da8656ce49adc85471101ed27f37c31a72
root <jinmingteo95@gmail.com>
2022-03-18 03:06:35 +0000

    [Dist_train] addition of visible GPU

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -15,8 +15,8 @@ PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 
 if [ ${GPUS} == 1 ]; then
-    python $(dirname "$0")/train.py  $CONFIG --work-dir=${WORK_DIR} ${@:4}
+    python tools/train.py  $CONFIG --work-dir=${WORK_DIR} ${@:4}
 else
-    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-        $(dirname "$0")/train.py $CONFIG --work-dir=${WORK_DIR} --launcher pytorch ${@:4}
+    CUDA_VISIBLE_DEVICES=1,3 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+        tools/train.py $CONFIG --work-dir=${WORK_DIR} --launcher pytorch ${@:4}
 fi
