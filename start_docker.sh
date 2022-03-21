ec9db4d1c6b189cb2c8f0b9cd24cf58e94ab8e39
root <jinmingteo95@gmail.com>
2022-03-21 01:55:00 +0000

    [Jupyter] disable token and password

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,7 @@
 dir="$PWD"
 #python mmocr/utils/ocr.py ttttt.png --output demo/output.jpg --det FCE_CTW_DCNv2 --det-ckpt fcenet_r50dcnv2_fpn_1500e_ctw1500_20211022-e326d7ec.pth --recog SAR_CN --recog-ckpt sar_r31_parallel_decoder_chineseocr_20210507-b4be8214.pth
 
-#nohup jupyter notebook --no-browser --ip=0.0.0.0 --port 8416 --allow-root > logsfile.logs & disown
+#nohup jupyter notebook --no-browser --ip=0.0.0.0 --port 8416 --allow-root > logsfile.logs --NotebookApp.token='' --NotebookApp.password='' & disown
 
 docker run -it --gpus all -d -v $dir:/$dir -w $dir --name mmocr_chi --shm-size=256m --ipc=host --expose 8416 -p 8416:8416 mmocr
 #docker run -it  --runtime=nvidia -v $dir:/$dir -w $dir --expose 8416 -p 8416:8416 mmocr
